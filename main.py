a262040636dd3c4a67b446fbcb65970e0ae39c18
Jonathan Felix <123984244+JonaThFelix@users.noreply.github.com>
2023-05-27 21:15:45 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from Pessoa import Pessoa
+
+Pessoa1 = Pessoa('Inês Brasil',1998,'Indefinido','França')
+Pessoa2 = Pessoa('Zé da Manga',1942,'Masculino','Suiça')
+Pessoa3 = Pessoa('irineu Maia',1988,'Feminino','Holanda')
+
+print('-'*80)
+Pessoa1.apresentacao()
+Pessoa1.aposentadoria()
+
+print('-'*80)
+Pessoa2.apresentacao()
+Pessoa2.aposentadoria()
+
+print('-'*80)
+Pessoa3.apresentacao()
+Pessoa3.aposentadoria()
